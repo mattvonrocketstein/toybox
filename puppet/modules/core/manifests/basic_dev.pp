c675bba18c4230e4cdaaea4dde5e5a8d9c1899b0
mvr <matthewvonrocketstein@gmail-dot-com>
2014-10-30 04:25:59 -0400

    docs for the example

--- puppet/modules/core/manifests/basic_dev.pp
+++ puppet/modules/core/manifests/basic_dev.pp
@@ -1,16 +1,15 @@
 # puppet/modules/core/manifests/basic_dev.pp
 class core::basic_dev{
-  class{git:
-    svn => 'absent',
-    gui => 'absent',
-  }
-  #$basic_dev_vcs = ['git']
-  #package { $basic_dev_vcs:ensure => installed}
 
   $basic_dev_misc_tools = ['tree','nmap', 'screen', 'ack-grep']
   $basic_dev_ruby_base = ['ruby', 'ruby-dev', 'gem']
   package {$basic_dev_misc_tools:ensure => installed}
   package {$basic_dev_ruby_base:ensure => installed}
+
+  class { git:
+    svn => 'absent',
+    gui => 'absent',
+  }
 
   class { 'python' :
     version    => 'system',
--- puppet/modules/site/manifests/vcs_work.pp
+++ puppet/modules/site/manifests/vcs_work.pp
@@ -1,10 +1,18 @@
 # puppet/modules/site/manifests/vcs_work.pp
 #
+# puppet-vcsrepo sucks.  despite the popularity of git and the fact
+# that only git is officially supported, there is a two-year old bug
+# where this module will fetch on the network every time provisioning
+# happens, regardless of whether the repo already exists on the correct
+# branch.  below you can find an example of how to use the git module to
+# effect clones.  note that the git module does not support "user", so
+# don't forget to chown the repo afterwards if you want a non-root user.
+#
 class site::vcs_work{
-  git::repo{'pg':
-    path   => '/tmp/pg',
-    branch => 'master',
-    #source => 'git://example.org/example/repo.git'
-    source => 'https://github.com/nesi/puppet-git.git'
-  }-> exec {'chown -R vagrant:vagrant /tmp/pg':}
+ # git::repo{'puppet-git':
+ #   path   => '/tmp/puppet-git',
+ #   branch => 'master',
+ #   ##source => 'git://example.org/example/repo.git'
+ #   source => 'https://github.com/nesi/puppet-git.git'
+ # }-> exec {'chown -R vagrant:vagrant /tmp/pg':}
 }
