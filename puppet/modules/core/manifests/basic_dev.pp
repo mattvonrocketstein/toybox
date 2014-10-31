8ab277d80cce69a48b4c8dbbf6c2e44cc8666415
mvr <matthewvonrocketstein@gmail-dot-com>
2014-10-31 07:51:33 -0400

    add unzip

--- puppet/modules/core/manifests/basic_dev.pp
+++ puppet/modules/core/manifests/basic_dev.pp
@@ -1,7 +1,7 @@
 # puppet/modules/core/manifests/basic_dev.pp
 class core::basic_dev{
 
-  $basic_dev_misc_tools = ['tree','nmap', 'screen', 'ack-grep']
+  $basic_dev_misc_tools = ['tree','nmap', 'screen', 'unzip', 'ack-grep']
   $basic_dev_ruby_base = ['ruby', 'ruby-dev', 'gem']
   package {$basic_dev_misc_tools:ensure => installed}
   package {$basic_dev_ruby_base:ensure => installed}
