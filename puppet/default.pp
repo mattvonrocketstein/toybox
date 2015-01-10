c4cae3da0d3c97aef859b1a1b19865af5df97f41
mvr <matthewvonrocketstein@gmail-dot-com>
2015-01-10 03:15:19 +0000

    minor changes

--- puppet/default.pp
+++ puppet/default.pp
@@ -30,13 +30,15 @@ node default {
   file { '/etc/logstash/conf.d/logstash.conf':
     ensure  => file,
     content => template('site/logstash.conf.erb'),
+    require => Package['logstash']
   }
-  service { 'logstash':
-      ensure    => running,
-      enable    => true,
-      subscribe => File['/etc/logstash/conf.d/logstash.conf'],
-    }
+  #service { 'logstash':
+  #    ensure    => running,
+  #    enable    => true,
+  #    subscribe => File['/etc/logstash/conf.d/logstash.conf'],
+  #  }
   include core::basic_dev
+  include core::logstash
   include core::toybox
   include site::my_code
 
@@ -70,25 +72,6 @@ exec {
     require => Package['elasticsearch'],
     command => 'sudo update-rc.d elasticsearch defaults 95 10'
 }
-
-exec { "update_apt_for_logstash":
-    command => "(echo 'deb http://packages.elasticsearch.org/logstash/1.4/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list) && sudo apt-get update",
-    creates => "/etc/apt/sources.list.d/logstash.list"
-}
-#package { 'logstash=1.4.2-1-2c0f5a1': 
-#    ensure => present,
-#    install_options => '--force-yes',
-#    require => Exec['update_apt_for_logstash'],
-#}
-exec { "install_logstash":
-  command => "sudo apt-get install -y --force-yes logstash=1.4.2-1-2c0f5a1",
-}
-#class { "logstash":
-#  install             => "source",
-#  install_source      => "https://download.elasticsearch.org/logstash/logstash/logstash-1.3.3-flatjar.jar",
-#  version => '1.3.3',
-#  template => "site/logstash.conf.erb",
-#}
 
   if $vagrant_provision_xwin {
     include site::xwindows
